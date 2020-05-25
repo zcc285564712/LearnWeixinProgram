a9ee7bd692d8e04b92e553ea98b2d003d14d9054
zhengchenchen <chenchen.zheng@56pingtai.net>
2020-05-25 16:43:40 +0800

    分享功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,52 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 处理handleClickToast
+  handleClickToast(){
+    wx.showToast({
+      title: '加载中',
+      duration:5000,
+      icon: 'loading',
+      mask: true,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 处理handleClickModal
+  handleClickModal(){
+    wx.showModal({
+      title:"提示",
+      content: '确定删除吗',
+      // showCancel:false, // 是否显示取消按钮
+      success: function (res) {
+        if(res.cancel){
+          console.log('点击取消了')
+        }
+        if(res.confirm){
+          console.log('点击了确定')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleLoading(){
+    // 这个不能设置自动停止加载，如果要停止，要调用wx.hideLoading
+    wx.showLoading({
+      title: 'hh',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleActionSheet(){
+    wx.showActionSheet({
+      itemList: ['拍照','录像'],
+      success:function (res) {
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 分享功能
+  onShareAppMessage(){
+    return {
+      title: '你好',
+      path: '', // 如果不设置，默认点击后进入首页
+      imageUrl: '', // 分享时候显示的图片，不填默认是当前页面的截图
+    }
   }
+  
 })
