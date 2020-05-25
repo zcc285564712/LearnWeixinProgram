7aa53a42cdef3b8e4b6111a753769d4b27bc2130
zhengchenchen <chenchen.zheng@56pingtai.net>
2020-05-25 19:51:57 +0800

    跳转

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -1,27 +1,18 @@
-// pages/home/home.js
+// pages/comment/comment.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title: 'hh'
+
   },
-  // 点击按钮跳转
-  handleToDetail(){
-    // 跳转
-    wx.navigateTo({
-      url: '/pages/detail/detail?hobby="ball"&color="red"',
-    })
-    // wx.redirectTo({
-    //   url: 'url',
-    // })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,27 +1,34 @@
-// pages/home/home.js
+// pages/detail/detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title: 'hh'
+
   },
-  // 点击按钮跳转
-  handleToDetail(){
-    // 跳转
-    wx.navigateTo({
-      url: '/pages/detail/detail?hobby="ball"&color="red"',
+  // 点击按钮用js实现返回
+  handleBack(){
+    wx.navigateBack({
+      delta: 1
     })
-    // wx.redirectTo({
-    //   url: 'url',
-    // })
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // 获取页面跳转过来携带的值
+    console.log(options)
+  },
+  // 监听页面卸载
+  onUnload(){
+   const pages = getCurrentPages(); // 获取所有活跃的页面对象，当前页面和目标页面
+   console.log(pages)
+   const home = pages[pages.length - 2]; // 获取跳回的页面对象
+   home.setData({
+     title: '篮球'
+   })
   },
 
   /**
@@ -45,12 +52,6 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
